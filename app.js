db43888dd39e73440723d0366c081fd810e24c02
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-08-14 16:57:08 +0100

    connect to database

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
+
 
 app.set('view engine', 'ejs');//  selecting ejs to serve as the templating system.
 
@@ -9,9 +11,16 @@ app.use(express.static('public')); // directing express to a folder to use stati
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+const DBURI = "mongodb+srv://jay:zuzu@cluster0.zyzefdv.mongodb.net/" //make collection names plural
+mongoose.connect(DBURI, ({useNewUrlParser: true, useUnifiedTopology: true})) // connect to database
+    .then((result) => app.listen(8000))
+    .catch((err) => console.log(err));
+
 app.listen(3000, ()=> {
-    console.log("This app runs on port 3000")
+    console.log("This app runs on port 8000")
 })
+
+let newTodoList = [];
 
 
 app.get('/', (req, res)=> {
