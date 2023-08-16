1d46b9cc1418d3945f1beb2f6171c4f4959e395c
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-08-16 19:23:40 +0100

    create custom error responses

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,14 +3,21 @@ const User = require("../models/User");
 
 const handleErrors = (err) => {
     console.log(err.message, err.code);
-    let error = { email: '', password : '' };
+    let errors = { email: '', password : '' };
+
+    //duplicate err code fix
+    if (err.code === 11000) {
+        errors.email = 'This email already exists'
+        return errors;
+    };
 
     // validation errors using error string scan
     if (err.message.includes('user validation failed')) {
-        console.log(Object.values(err.errors).forEach(error => {
-            console.log(error.properties);
-        })); // gets value of the error object
+        Object.values(err.errors).forEach(({ properties }) => { //by destructuring, we get direct access to prop
+            errors[properties.path] = properties.message;
+        }); // gets value of the error object
     }
+    return errors
 }
 
 module.exports.signup_get = (req, res) => {
@@ -33,7 +40,8 @@ module.exports.signup_post = async (req, res) => {
         res.status(201).json(user); //send user back as json
     }
     catch(err) {
-        handleErrors(err)
+        const errors = handleErrors(err);
+        res.status(400).json({ errors })
     }
 }
 
