890cea2a73e47e0593db9a65e8c34035881d3c17
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-08-18 20:35:08 +0100

    create login and signup views

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,54 +1,55 @@
 const User = require("../models/User");
 
-
+// handle errors
 const handleErrors = (err) => {
-    console.log(err.message, err.code);
-    let errors = { email: '', password : '' };
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
 
-    //duplicate err code fix
-    if (err.code === 11000) {
-        errors.email = 'This email already exists'
-        return errors;
-    };
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = 'that email is already registered';
+    return errors;
+  }
 
-    // validation errors using error string scan
-    if (err.message.includes('user validation failed')) {
-        Object.values(err.errors).forEach(({ properties }) => { //by destructuring, we get direct access to prop
-            errors[properties.path] = properties.message;
-        }); // gets value of the error object
-    }
-    return errors
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    // console.log(err);
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
 }
 
+// controller actions
 module.exports.signup_get = (req, res) => {
-    res.render('signup');
+  res.render('signup');
 }
 
 module.exports.login_get = (req, res) => {
-    res.render('login')
+  res.render('login');
 }
 
 module.exports.signup_post = async (req, res) => {
-    const { email, password } = req.body; // the email and password inputs were put in using postman, to test the server 
-    // functionalities without the presence of the frontend.
-    // and then it is destructured using the curly braces and set to req.body using the express.json() middleware which
-    // was imported into the app.js file
+  const { email, password } = req.body;
 
-    try {
-        const user = await User.create({ email, password }) // creates an instance of a user and passess it into the database.
-        // we pass in the object containing the necessities for user attributes from the userSchema created
-        res.status(201).json(user); //send user back as json
-    }
-    catch(err) {
-        const errors = handleErrors(err);
-        res.status(400).json({ errors })
-    }
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  }
+  catch(err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
+ 
 }
 
 module.exports.login_post = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    console.log(email, password);
-
-    res.send('user login');
+  console.log(email, password);
+  res.send('user login');
 }
--- models/User.js
+++ models/User.js
@@ -1,47 +1,34 @@
-const mongoose = require("mongoose");
-const { isEmail } = require("validator");
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 const bcrypt = require("bcrypt");
 
-
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: [true, 'please enter an email'],
-        unique: true,
-        lowercase: true,
-        validate: [isEmail, 'Please enter a valid email'] //automatically checks email input for validity of email
-    },
-    password: {
-        type: String,
-        required: [true, 'please enter a password'],
-        minlength: [6, 'min password length is 6 characters'], // minlength should not be camelCase
-    },
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  }
 });
-// mongoose hooks
-//execute a function before doc is saved to schema
-userSchema.pre('save', async function(next) {
-    console.log("Here we go, weeeeee!", this); // this represents the instance of the user
-    // generate a salt
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-})
 
+// fire a function after doc saved to db
+userSchema.post('save', function (doc, next) {
+  console.log('new user was created & saved', doc);
+  next();
+});
 
-//execute a function after a new user has been saved to database
-userSchema.post('save', function(doc, next) {
-    console.log('new user created', doc);
+// fire a function before doc saved to db
+userSchema.pre('save', function (next) {
+  console.log('user about to be created & saved', this);
+  next();
+});
 
-    next();
-})
-
-
-
-
-
-// create a new model based on the userSchema
-const User = mongoose.model('user', userSchema); //must be in singular form to what you named your db collection. 
-// mongoose looks at the model name 'user', pluralizes it and then connects it to your collection.
-
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
