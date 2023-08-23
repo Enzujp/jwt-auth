302f43b5dc4097f0c1c43e5ab142407d6cc963f8
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-08-23 14:31:29 +0100

    initialize logout function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddlewares');
 
 const app = express();
 
@@ -21,7 +22,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // routes
 app.get('/', (req, res) => res.render('index'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 
